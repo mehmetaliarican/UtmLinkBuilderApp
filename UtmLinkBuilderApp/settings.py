991bb2139b5a7f707aea7aa6cc06406799c56f6a
Mehmet Ali Arican <mehmet@canikocorp.com>
2024-06-21 22:49:49 +0100

    gunicorn back

--- UtmLinkBuilderApp/settings.py
+++ UtmLinkBuilderApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-saa))ecg+c^nmm!*celgd$qkbi7z%6sl1o&b6e!o)qg#(*+4&1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
